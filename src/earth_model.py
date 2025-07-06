3f13079851cec9493a3a47be828be7c3d179f46a
VimsRocz <96129555+VimsRocz@users.noreply.github.com>
2025-07-06 22:09:55 +0200

    Revert "Fix gravity integration sign"

--- src/earth_model.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-from .utils import normal_gravity
-
-
-def gravity_ned(lat_rad: float, h_m: float = 0.0) -> np.ndarray:
-    """Return gravity vector in **NED** (+Down) [m/s²]."""
-    g = normal_gravity(lat_rad, h_m)
-    return np.array([0.0, 0.0, +g])   # +g is down in NED
