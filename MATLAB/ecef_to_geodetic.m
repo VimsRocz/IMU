4e0aacaf8a15bef5b6316c9e137ad2acf7593e1e
VimsRocz <96129555+VimsRocz@users.noreply.github.com>
2025-07-27 19:33:35 +0200

    Add vectorized ECEF to geodetic conversion

--- MATLAB/ecef_to_geodetic.m
+++ MATLAB/ecef_to_geodetic.m
@@ -1,6 +1,35 @@
-function varargout = ecef_to_geodetic(varargin)
-%ECEF_TO_GEODETIC Compatibility wrapper for ecef2geodetic.
-%   [LAT_DEG, LON_DEG, ALT] = ECEF_TO_GEODETIC(X,Y,Z) forwards all arguments
-%   to ECEF2GEODETIC so older scripts continue to function.
-    [varargout{1:nargout}] = ecef2geodetic(varargin{:});
+function [lat_deg, lon_deg, h] = ecef_to_geodetic(x, y, z)
+%ECEF_TO_GEODETIC  Convert ECEF coordinates to geodetic latitude, longitude and height.
+%% Converts ECEF coordinates to geodetic coordinates (lat, lon, h) using WGS84.
+%   [LAT_DEG, LON_DEG, H] = ECEF_TO_GEODETIC(X, Y, Z) converts Earth-Centered
+%   Earth-Fixed (ECEF) coordinates ``X``, ``Y`` and ``Z`` (in metres) to the
+%   corresponding geodetic latitude, longitude (degrees) and height above the
+%   WGS‑84 ellipsoid (metres).  Inputs may be scalars or arrays of equal size and
+%   the outputs will match that size.
+%
+%   This implementation does not require the Mapping Toolbox.  When the toolbox
+%   is available, the built-in ``ecef2geodetic`` could be used instead.
+
+% WGS‑84 ellipsoid parameters
+    a = 6378137.0;               % semi-major axis [m]
+    e = 0.081819190842622;       % first eccentricity
+
+    e_sq = e^2;
+    p = sqrt(x.^2 + y.^2);
+
+    b = a * sqrt(1 - e_sq);
+    theta = atan2(z .* a, p .* b);
+    ep_sq = (a^2 - b^2) / b^2;
+
+    lon = atan2(y, x);
+    lat = atan2( ...
+        z + ep_sq * b .* sin(theta).^3, ...
+        p - e_sq * a .* cos(theta).^3);
+
+    N = a ./ sqrt(1 - e_sq * sin(lat).^2);
+    h = p ./ cos(lat) - N;
+
+    lat_deg = rad2deg(lat);
+    lon_deg = rad2deg(lon);
 end
+
