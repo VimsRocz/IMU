39f2040b359e1dca5cd03aabf8eed749971114c0
VimsRocz <96129555+VimsRocz@users.noreply.github.com>
2025-07-27 12:48:21 +0200

    Remove redundant MATLAB files from src

--- src/run_triad_only.m
+++
@@ -1,9 +0,0 @@
-function run_triad_only()
-%RUN_TRIAD_ONLY Entry point mirroring src/run_triad_only.py
-%   RUN_TRIAD_ONLY() executes run_all_datasets_matlab('TRIAD').
-%
-% Usage:
-%   run_triad_only
-
-    run_all_datasets_matlab('TRIAD');
-end
