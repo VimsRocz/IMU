9d9b2f69cff6b23558808f173f4f67c9fe0ec158
VimsRocz <96129555+VimsRocz@users.noreply.github.com>
2025-08-12 21:55:25 +0200

    feat: add MATLAB figure saving

--- MATLAB/save_plot_fig.m
+++ MATLAB/save_plot_fig.m
@@ -1,5 +1,15 @@
-function save_plot_fig(~, ~)
-%SAVE_PLOT_FIG  Placeholder for saving plots in MATLAB .fig format.
-%   Mirrors the Python ``save_plot_fig`` helper but is not implemented yet.
-error('save_plot_fig not yet implemented');
+function save_plot_fig(fig, filename)
+%SAVE_PLOT_FIG  Save figure in MATLAB ``.fig`` format.
+%   SAVE_PLOT_FIG(FIG, FILENAME) writes the given figure to *FILENAME*
+%   using MATLAB's :func:`savefig` so it can be reopened later.  This
+%   mirrors the Python ``save_plot_fig`` helper.
+
+    if nargin < 1 || isempty(fig)
+        fig = gcf; %#ok<GFLD> default to current figure if none provided
+    end
+    if nargin < 2 || isempty(filename)
+        error('A filename must be provided');
+    end
+
+    savefig(fig, filename);
 end
