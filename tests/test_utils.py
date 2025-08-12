ea0f45027b2b1d444e4773da6e72d2e822ed9f8d
VimsRocz <96129555+VimsRocz@users.noreply.github.com>
2025-08-12 13:33:13 +0200

    Handle empty input in zero_base_time

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from src.utils import compute_C_ECEF_to_NED
+from src.utils import compute_C_ECEF_to_NED, zero_base_time
 
 np = pytest.importorskip("numpy")
 
@@ -11,3 +11,9 @@ def test_rotation_matrix_orthonormal():
     # Orthonormal check
     identity = C @ C.T
     assert np.allclose(identity, np.eye(3), atol=1e-6)
+
+
+def test_zero_base_time():
+    assert zero_base_time([]).size == 0
+    t = np.array([5.0, 6.5, 7.0])
+    assert np.allclose(zero_base_time(t), t - 5.0)
